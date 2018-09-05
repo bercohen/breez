9c9c135bd8a07cadb538435f004266aada180ac7
Sholom Ber Cohen <thegreatber@gmail.com>
2018-09-04 23:32:58 -0400

    tweaked migration

--- db/migrate/20180831204543_create_line_items.rb
+++ db/migrate/20180831204543_create_line_items.rb
@@ -1,8 +1,8 @@
 class CreateLineItems < ActiveRecord::Migration[5.0]
   def change
     create_table :line_items do |t|
-      t.references :cart, foreign_key: true
-      t.references :product, foreign_key: true
+      t.integer :cart_id
+      t.integer :product_id
       t.integer :qty, default: 0
 
       t.timestamps
